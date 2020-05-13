20cebe6f983b042ff9e691c1b48af4865694ab35
Ari Nugraha <nugrahaa878@gmail.com>
2020-05-12 21:37:41 -0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/MyAc/matkulservice/MatkulserviceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.MyAc.matkulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatkulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatkulserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/MyAc/matkulservice/MatkulserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.MyAc.matkulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatkulserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
